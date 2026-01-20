2cabda8e20bf659233a5af560381015601227e6e
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-20 02:40:08 -0500

    refactor(create): convert POST route from .then .catch chain to async/await function with try catch

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -147,17 +147,16 @@ router.put("/products/:id", async (req, res) => {
 // CREATE - post a new product, set route and callback to check bookleans used in schema
 router.post("/products", async (req, res) => {
     // create a book, req.body refers to whatever data is passed from the client/ui form
-    Product.create(req.body)
-        .then(createdProduct => {
-            console.log("New product successfully created", createdProduct);
-            // response w success code for post method
-            res.status(201).json(createdProduct)
-        })
-        .catch(error => {
-            console.error("Error adding product", error);
-            // response with bad request code if product doesnt meet the requirements in the schema - send the detailed validation error from mongoose as a json object
-            res.status(400).json({ message: error.message });
-        })
+    try {
+        const createdProduct = await Product.create(req.body);
+        console.log("New product successfully created", createdProduct);
+        // response w success code for post method
+        res.status(201).json(createdProduct)
+    } catch (error) {
+        console.error("Error adding product", error);
+        // response with bad request code if product doesnt meet the requirements in the schema - send the detailed validation error from mongoose as a json object
+        res.status(400).json({ message: error.message });
+    }
 });
 
 module.exports = router;
